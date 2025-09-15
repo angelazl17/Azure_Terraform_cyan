6098b3c6752e4c9a0511f6fb31469e06497bb624
li.zhao <angelazl@yahoo.com>
2025-09-15 16:01:37 -0500

    azure with first network config

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "environment" {
+  description = "The deployment environment (e.g., dev, stg, prod)"
+  type        = string
+  default     = "dev"
+}
+
+
+variable "company_name" {
+  description = "The code of the machine learning"
+  type        = string
+  default     = "cyan"
+}
